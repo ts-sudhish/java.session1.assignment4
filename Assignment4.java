9fc2793fe289d135d0ebaa198a89eaf65eb9a428
ts-sudhish <sudhish.soss@gmail.com>
2016-06-15 13:25:05 +0000

    Add files via upload

---
+++ Assignment4.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package sdh.java.session1;
+
+import java.util.Scanner;
+
+/**
+ * @author sudhi
+ * 
+ */
+public class Assignment4 {
+
+	/**
+	 * @param args
+	 */
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x = 5, y = 10;
+		System.out.println("value of x = " + x);
+		System.out.println("value of y = " + y);
+
+		int ans = x + y * 2;
+		System.out.println("value of [x + y * 2] = " + ans);
+
+		ans = x - y + 2;
+		System.out.println("value of [x - y + 2] = " + ans);
+
+		ans = (x + y) * 2;
+		System.out.println("value of [(x+y) * 2] = " + ans);
+
+		ans = y % x;
+		System.out.println("value of [y % x] = " + ans);
+
+	}
+
+}
